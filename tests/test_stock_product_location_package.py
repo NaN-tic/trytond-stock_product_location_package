2cb6322504586a25db427d1f02e7faaeb12e5d0b
jmartin <jmartin@zikzakmedia.com>
2014-11-07 14:47:31 +0100

    Cleanup tests

--- tests/test_stock_product_location_package.py
+++ tests/test_stock_product_location_package.py
@@ -6,31 +6,21 @@ import os
 import sys
 import trytond.tests.test_tryton
 import unittest
-DIR = os.path.abspath(os.path.normpath(os.path.join(__file__,
-    '..', '..', '..', '..', '..', 'trytond')))
-if os.path.isdir(DIR):
-    sys.path.insert(0, os.path.dirname(DIR))
 
 
 class StockProductLocationPackageTestCase(unittest.TestCase):
-    '''
-    Test Stock Product Location Package module.
-    '''
+    'Test Stock Product Location Package module'
 
     def setUp(self):
         trytond.tests.test_tryton.install_module(
             'stock_product_location_package')
 
     def test0005views(self):
-        '''
-        Test views.
-        '''
+        'Test views'
         test_view('stock_product_location_package')
 
     def test0006depends(self):
-        '''
-        Test depends.
-        '''
+        'Test depends'
         test_depends()
 
 
