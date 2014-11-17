24b9afeda034fb7b982759b2a8ef9d3e6701c8b6
resteve <resteve@zikzakmedia.com>
2014-11-17 11:40:35 +0100

    Location rack, row and case in move (by warehouse)

---
+++ move.py
@@ -0,0 +1,82 @@
+# This file is part of stock_product_location_package module for Tryton.
+# The COPYRIGHT file at the top level of this repository contafroms
+# the full copyright notices and license terms.
+from trytond.model import fields
+from trytond.pool import Pool, PoolMeta
+
+__all__ = ['Move']
+__metaclass__ = PoolMeta
+
+
+class Move:
+    __name__ = 'stock.move'
+    from_location_rack = fields.Function(fields.Char('From Location Rack'),
+        'get_from_location_wharehose')
+    from_location_row = fields.Function(fields.Char('From Location Row'),
+        'get_from_location_wharehose')
+    from_location_case = fields.Function(fields.Char('From Location Case'),
+        'get_from_location_wharehose')
+    to_location_rack = fields.Function(fields.Char('To Location Rack'),
+        'get_to_location_wharehose')
+    to_location_row = fields.Function(fields.Char('To Location Row'),
+        'get_to_location_wharehose')
+    to_location_case = fields.Function(fields.Char('To Location Case'),
+        'get_to_location_wharehose')
+
+    def get_from_location_wharehose(self, name):
+        '''Location (rack-row-case) in From Location and Warehouse'''
+        ProductLocation = Pool().get('stock.product.location')
+
+        if not self.from_location:
+            return None
+        if not self.from_location.parent:
+            return None
+        if not self.from_location.parent.type == 'warehouse':
+            return None
+        if not self.product:
+            return None
+
+        prod_locations = ProductLocation.search([
+            ('product', '=', self.product),
+            ('warehouse', '=', self.from_location.parent),
+            ('location', '=', self.from_location),
+            ], limit=1)
+        if not prod_locations:
+            return None
+        prod_location, = prod_locations
+        if name == 'from_location_rack':
+            return prod_location.loc_rack
+        if name == 'from_location_row':
+            return prod_location.loc_row
+        if name == 'from_location_case':
+            return prod_location.loc_case
+        return None
+
+    def get_to_location_wharehose(self, name):
+        '''Location (rack-row-case) in To Location and Warehouse'''
+        ProductLocation = Pool().get('stock.product.location')
+
+        if not self.to_location:
+            return None
+        if not self.to_location.parent:
+            return None
+        if not self.to_location.parent.type == 'warehouse':
+            return None
+        if not self.product:
+            return None
+
+        prod_locations = ProductLocation.search([
+            ('product', '=', self.product),
+            ('warehouse', '=', self.to_location.parent),
+            ('location', '=', self.to_location),
+            ], limit=1)
+        if not prod_locations:
+            return None
+        prod_location, = prod_locations
+        if name == 'to_location_rack':
+            return prod_location.loc_rack
+        if name == 'to_location_row':
+            return prod_location.loc_row
+        if name == 'to_location_case':
+            return prod_location.loc_case
+        return None
