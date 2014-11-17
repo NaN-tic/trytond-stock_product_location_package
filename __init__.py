33ae6601796123e1682d81f26614c78a6e198a66
resteve <resteve@zikzakmedia.com>
2014-11-17 11:40:35 +0100

    Location rack, row and case in move (by warehouse)

--- __init__.py
+++ __init__.py
@@ -2,12 +2,13 @@
 # The COPYRIGHT file at the top level of this repository contains
 # the full copyright notices and license terms.
 from trytond.pool import Pool
+from .move import *
 from .location import *
 from .product import *
 
-
 def register():
     Pool.register(
+        Move,
         Product,
         ProductLocation,
         module='stock_product_location_package', type_='model')
